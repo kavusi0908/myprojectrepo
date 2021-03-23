1294714b5e9f13864b1a3993bb2c973fd1140a3a
kavusi0908 <akpartners98@gmail.com>
2021-03-23 10:53:47 +0530

    added br1 function

--- SampleGitDemo/src/main/Calculator.java
+++ SampleGitDemo/src/main/Calculator.java
@@ -36,5 +36,7 @@ public class Calculator {
 	public static void four(int num1){
 	      System.out.println(num1*num1*num1*num1);
 	  }
-
+	public static void br1(int num1){
+	      System.out.println(num1);
+	  }
 }
